b5f95e61a13a688149ff3a21e1fba88a1db2badf
Dr Humayun <ishfaqmuzaffar@gmail.com>
2026-03-26 17:40:45 +0530

    Fix CORS properly using Nest enableCors

--- src/main.ts
+++ src/main.ts
@@ -4,25 +4,16 @@ import { AppModule } from './app.module';
 import { existsSync, mkdirSync } from 'fs';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  const corsOptions: cors.CorsOptions = {
-    origin: [
-      'http://72.60.28.22:8080',
-      'http://localhost:8080',
-      'http://127.0.0.1:8080',
-    ],
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+  app.enableCors({
+    origin: ['http://72.60.28.22:8080'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
-    optionsSuccessStatus: 204,
-  };
-
-  app.use(cors(corsOptions));
-  app.enableCors(corsOptions);
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
