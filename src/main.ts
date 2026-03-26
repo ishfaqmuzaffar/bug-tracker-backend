b11a818491ded842c3edf890ae6d1a48064fc8f2
Dr Humayun <ishfaqmuzaffar@gmail.com>
2026-03-26 12:37:16 +0530

    fix cors for frontend origin

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: [
+      'http://72.60.28.22:8080',
+      'http://localhost:8080',
+      'http://localhost:3000',
+      'http://127.0.0.1:8080',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: false,
+  });
+
+  await app.listen(3000, '0.0.0.0');
 }
+
 bootstrap();
